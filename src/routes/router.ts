f6d5201cdbaccfd0e3ba89d0ed2176746faee4c0
zachspiel <zachspielberger@gmail.com>
2022-01-26 06:08:21 -0700

    move public directory to right location

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Response } from 'express';
 import path from 'path';
 import controller from '../controllers/controller';
 const fs = require('fs');
@@ -15,35 +15,40 @@ const avaialableFolders = [
 router.get('/', controller.getRootData);
 router.get('/:route', controller.getAllData);
 router.get('/:route/:id', controller.getSpecificItem);
-router.get('/images/:folder/:file', (req, res) => {
+router.get('/images/:folder/:file', (req, res: Response) => {
   const { file, folder } = req.params;
   const errorMessage =
     file === undefined ? 'No image was provided in url' : `Image ${file} was not found`;
 
   if (avaialableFolders.includes(req.params.folder)) {
-    const targetDirectory = path.join(__dirname, '../public/images', folder);
-    let totalImages = getTotalImages(targetDirectory.toString());
-
+    const targetDirectory = path.join(__dirname, '../../public/images', folder);
     const targetId = file.split('.')[0];
+    let totalImages = getTotalImages(targetDirectory.toString());
+    const outOfBoundsError = `Image ${file} is outside of bounds ${totalImages} for directory ${folder}`;
+
     if (!Number.isNaN(Number(targetId)) && Number(targetId) <= totalImages) {
       const filePath = path.join(targetDirectory, file);
       return res.sendFile(path.resolve(filePath));
     }
-    return res.status(404).json({
-      message: `Image ${file} is outside of bounds ${totalImages} for directory ${folder}`,
-    });
+
+    sendErrorMessage(res, outOfBoundsError);
   } else {
-    const error = new Error(errorMessage);
-    return res.status(404).json({
-      message: error.message,
-    });
+    sendErrorMessage(res, errorMessage);
   }
 });
 
 const getTotalImages = (folder: string): number => {
-  const totalImages = fs.readdirSync(folder).length;
+  try {
+    return fs.readdirSync(folder).length;
+  } catch (err) {
+    return 0;
+  }
+};
 
-  return totalImages;
+const sendErrorMessage = (response: Response, message: string) => {
+  return response.status(404).json({
+    message: message,
+  });
 };
 
 export = router;
